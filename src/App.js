ceb1324739d51a517cb724ee59dd9e5653056663
BSGMG18 <maximilianglomm@googlemail.com>
2019-12-14 15:06:16 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from "react";
-import "./App.css";
+import RecursiveProperty from "./RecursiveProperty";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-intro">
+        <RecursiveProperty
+          property={testJson}
+          propertyName="Speisekarte"
+          rootProperty={true}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
+
+const testJson = [
+  {
+    id: "1",
+    name: "null",
+    email: "hel",
+    kinder: {
+      id: "1.1",
+      name: "hallo",
+      email: "nice",
+      kinder: { id: "1.1.1", name: "men", email: "mento", kinder: {} }
+    }
+  },
+  { id: "2", name: "hell", email: "tothe", Hallo: {} },
+  {
+    id: "3",
+    name: "hell",
+    email: "tothe",
+    kinder: { id: "3.1", name: "hell", email: "tothe", kinder: {} }
+  }
+];
